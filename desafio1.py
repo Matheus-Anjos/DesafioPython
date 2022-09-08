f244be984a243f561739b8ec2cca9f48e40a8106
Matheus <matheus.mfa10@gmail.com>
2022-09-08 13:50:30 -0300

    [@Matheus-Anjos Desafio Logica Python]

---
+++ desafio1.py
@@ -0,0 +1,33 @@
+#cria a lista com os números
+n = int(input("Quantos números você irá querer na lista?"))
+lista = [0] * n
+for i in range(n) :
+    lista[i] = int(input(i, "item da sua lista"))
+
+#declara ad variaveis do menor número possível e menor número da lista para comparação
+menor_numero_possivel = 0
+menor_numero_lista = lista[0]
+
+#declara os incrementos que serão utilizados nos loops
+i = 0
+j = 0
+
+#encontra o menor número da lista e seu índice
+for i in range(len(lista)) :
+    if ( lista[i] < menor_numero_lista) :
+        menor_numero_lista = lista[i]
+        indice_menor_numero = i
+
+#caso o menor número não tenha um sucessor, este será o menor número da lista
+if not (lista[indice_menor_numero] + 1) in lista :
+            menor_numero_possivel = lista[indice_menor_numero] + 1
+
+#caso o menor número tenha um sucessor, verificar se próximos que o sucedem tem um sucessor, caso não tenha, este será o menor número possível da lista de inteiros que não pertence a lista
+else :
+   for j in range(len(lista)) :
+       if (lista[indice_menor_numero] + j) in lista and not (lista[indice_menor_numero] + j+1) in lista :
+        menor_numero_possivel = lista[indice_menor_numero] + j+1 
+        break
+
+#imprimi os valores
+print("o menor número inteiro possível que não pertence a lista é " , menor_numero_possivel)
